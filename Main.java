e6c2905ff0d706f75cdca660967608e1947a6963
Kim Rader <kimrader@gmail.com>
2019-03-21 16:21:40 -0700

    Update Main.java
    Simplified isPrimeImproved method and made it run faster.

--- Main.java
+++ Main.java
@@ -1,28 +1,25 @@
 import java.util.*;
 
 class Main {
-//  public static final long[] numbersToTest = {-10, 0, 1, 2, 3, 4, 9, 11, 3169, 10000};
-  public static final long[] numbersToTest = {5915587277l};
-
+  public static final long[] numbersToTest = {-10l, 0l, 1l, 2l, 3l, 4l, 9l, 11l, 3169l, 10000l};
+//  public static final long[] numbersToTest = {5915587277l};
+//  public static final long[] numbersToTest = {3169l};
+  
   public static void main(String[] args) {
-    long startTime;
-    long endTime;
+    for (int i=0; i<numbersToTest.length; i++) {
+      long startTime = System.nanoTime();
+      System.out.print("Is " + numbersToTest[i] + " prime? " + isPrimeTrialAndError(numbersToTest[i]));
+      long endTime = System.nanoTime();
+      System.out.println("  Elapsed time: " + (endTime - startTime));
+    }
 
     System.out.println();
 
-    // for (int i=0; i<numbersToTest.length; i++) {
-    //   startTime = System.nanoTime();
-    //   System.out.println("Is " + numbersToTest[i] + " prime? " + isPrimeTrialAndError(numbersToTest[i]));
-    //   endTime = System.nanoTime();
-    //   System.out.println("Elapsed time: " + (endTime - startTime));
-    //   System.out.println();
-    // }
-
     for (int i=0; i<numbersToTest.length; i++) {
-      startTime = System.nanoTime();
-      System.out.println("Is " + numbersToTest[i] + " prime? " + isPrimeImproved(numbersToTest[i], new TreeSet()));
-      endTime = System.nanoTime();
-      System.out.println("Elapsed time: " + (endTime - startTime));
+      long startTime = System.nanoTime();
+      System.out.print("Is " + numbersToTest[i] + " prime? " + isPrimeImproved(numbersToTest[i]));
+      long endTime = System.nanoTime();
+      System.out.println("  Elapsed time: " + (endTime - startTime));
     }
   }
 
@@ -53,50 +50,32 @@ Improve the efficiency of the previous function in two ways:
 -only check potential factors that are themselves prime
 -this method is known as the "Sieve of Erastothenes"
 */
-  // assumption is that all primes up to the square root of num are in the knownPrimes list
-  private static boolean isPrimeImproved(long num, SortedSet<Long> knownPrimes) {
-System.out.println("calling isPrimeImproved(" + num + ", " + knownPrimes);    
-    // negative numbers, 0, and 1 are not considered prime by definition
-    if (num <= 1)
-      return false;
-
-    long highestKnownPrime = (knownPrimes.size() == 0 ? 2 : knownPrimes.last());
-    if (num < highestKnownPrime)
-      return false;
-
-    // iterate from 2 to the square root of our number
-    for (long potentialFactor=2; (potentialFactor*potentialFactor)<=num; potentialFactor++) {
-      if (!knownPrimes.contains(potentialFactor) && isPrimeImproved(potentialFactor, knownPrimes)) {
-//        System.out.println("Adding " + potentialFactor + " to list of known primes");
-        knownPrimes.add(potentialFactor);
-//        System.out.println("Known primes: " + knownPrimes);
-      }
-
-      if (knownPrimes.contains(potentialFactor)) {
-        System.out.println("Checking " + num + " modulo " + potentialFactor);
-        if (num % potentialFactor == 0)
+    public static boolean isPrimeImproved(long num) {
+        // negative numbers, 0, and 1 are not considered prime by definition
+        if (num <= 1)
           return false;
-      }
+
+        List<Long> knownPrimes = new ArrayList();
+        for (long potentialFactor=2; (potentialFactor*potentialFactor)<=num; potentialFactor++) {
+          if (isPrimeImprovedHelper(potentialFactor, knownPrimes)) {
+            knownPrimes.add(potentialFactor);
+            if (num % potentialFactor == 0)
+                return false;
+          }
+        }
+        return true;
+    }
+
+  // num must be an integer greater than 1
+  // knownPrimes must contain all primes up to the square root of num
+  private static boolean isPrimeImprovedHelper(long num, List<Long> knownPrimes) {
+    //System.out.println("calling isPrimeImprovedHelper(" + num + ", " + knownPrimes);    
+    for (long primeFactor : knownPrimes) {
+        if (num % primeFactor == 0)
+            return false;
+        if (primeFactor * primeFactor > num)
+            return true;
     }
     return true;
-
-
-  //     for (int i=0; i < knownPrimes.size(); i++) {
-  //       int curPrime = knownPrimes.get(i);
-
-  //       System.out.println("Checking " + potentialFactor + " mod " + curPrime);
-  //       if ((curPrime * curPrime > potentialFactor) || (potentialFactor % curPrime == 0)) {
-  //         isPotentialFactorPrime = false;
-  //         continue;   // hop out of this for loop
-  //       }
-  //     }
-  //     if (isPotentialFactorPrime) {
-  //       knownPrimes.add(potentialFactor);
-  //       System.out.println("Adding " + potentialFactor + " to primes list");
-  //     }
-  //   if (num % potentialFactor == 0) // if num modulo potentialFactor is zero
-  //     return false;                 // then potentialFactor is an actual factor and this num is not prime      
-  //   // no factors have been found, so this number must be prime
-  //   return true;
-  }
+    }
 }
